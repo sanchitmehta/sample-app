e881fede288b34683418899b6ea7bd7062504fbc
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 09:35:37 -0800

    Fix memory leaks

--- app/Models/HomeModel.cs
+++ app/Models/HomeModel.cs
@@ -1,9 +1,13 @@
+```csharp
+using System;
 using System.Collections.Generic;
 
 namespace SampleApp.Models
 {
-    public class HomeModel
+    public class HomeModel : IDisposable
     {
+        private bool _disposed;
+
         public string Title { get; set; }
         public string Description { get; set; }
         public List<string> Features { get; set; }
@@ -12,13 +16,81 @@ namespace SampleApp.Models
         public HomeModel()
         {
             Features = new List<string>();
+            Products = new List<Product>();
+        }
+
+        // Implement IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                // Dispose managed resources
+                if (Features != null)
+                {
+                    Features.Clear();
+                    Features = null;
+                }
+
+                if (Products != null)
+                {
+                    Products.Clear();
+                    Products = null;
+                }
+            }
+
+            // Clean up unmanaged resources here if any
+
+            _disposed = true;
+        }
+
+        ~HomeModel()
+        {
+            Dispose(false);
         }
     }
 
-    public class Product
+    public class Product : IDisposable
     {
+        private bool _disposed;
+
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+
+        // Implement IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                // Dispose managed resources here if any
+                Name = null;
+            }
+
+            // Clean up unmanaged resources here if any
+
+            _disposed = true;
+        }
+
+        ~Product()
+        {
+            Dispose(false);
+        }
     }
 }
+```
