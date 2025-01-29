71c38f639592ae87542f675a50aa629f75441f99
Matthew Nilsen <35314033+msnilsen@users.noreply.github.com>
2025-01-29 14:52:56 -0800

    Update for Managed Identity implementation

--- app/Program.cs
+++ app/Program.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using System.Data.SqlClient;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        var connectionString = "Server=unspecified.sql.server;Database=unspecified_database;Authentication=Active Directory Default;TrustServerCertificate=True;";
+        var credential = new DefaultAzureCredential();
+
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.AccessToken = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/" })).Token;
+            connection.Open();
+            // Perform database operations here if required
+        }
+
         CreateHostBuilder(args).Build().Run();
     }
 
@@ -14,5 +26,4 @@ public class Program
             {
                 webBuilder.UseStartup<Startup>();
             });
-
 }
