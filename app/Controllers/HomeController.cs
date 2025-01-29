47f592d867a5e80405d2a7b8746ca69e86cf1388
Matthew Nilsen <35314033+msnilsen@users.noreply.github.com>
2025-01-29 14:51:49 -0800

    Update for Managed Identity implementation

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Azure.Identity;
 using SampleApp.Models;
 using System;
 using System.Collections.Generic;
@@ -9,15 +10,12 @@ namespace SampleApp.Controllers
     public class HomeController : Controller
     {
         private readonly string connectionString;
+        private readonly DefaultAzureCredential azureCredential;
 
         public HomeController()
         {
-            var envConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
-            if (string.IsNullOrEmpty(envConnectionString))
-            {
-                throw new InvalidOperationException("Environment variable SQL_CONNECTION_STRING is not set.");
-            }
-            connectionString = envConnectionString;
+            connectionString = "Server=unspecified.sql.server;Database=unspecified_database;Authentication=Active Directory Default;TrustServerCertificate=True;";
+            azureCredential = new DefaultAzureCredential();
         }
 
         public IActionResult Index()
@@ -35,6 +33,10 @@ namespace SampleApp.Controllers
 
             using (var connection = new SqlConnection(connectionString))
             {
+                connection.AccessToken = azureCredential.GetToken(
+                    new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/.default" })
+                ).Token;
+
                 connection.Open();
                 var command = new SqlCommand("SELECT TOP 10 ProductId, Name, ListPrice FROM [SalesLT].[Product]", connection);
                 using (var reader = command.ExecuteReader())
