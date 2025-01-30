48d9cb945a67cc07550f08a005efd71b42c26f8b
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-29 20:03:19 -0800

    Revert "Implement SQL Managed Identity Authentication"

--- app/Program.cs
+++ app/Program.cs
@@ -1,22 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Azure.Identity;
-using System.Data.SqlClient;
-using System.Data;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        using (var connection = new SqlConnection("Server=oa-demo-sql-sanchit.database.windows.net;Database=products;Authentication=Active Directory Default;TrustServerCertificate=True;"))
-        {
-            var credential = new DefaultAzureCredential();
-            connection.AccessToken = credential.GetToken(new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/" })).Token;
-            connection.Open();
-
-            // Application logic here if needed
-        }
-
         CreateHostBuilder(args).Build().Run();
     }
 
