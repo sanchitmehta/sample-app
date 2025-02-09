56e03a86801c2f6792fa4fba7be3c972138001e2
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-02-09 12:20:33 -0800

    Update for Managed Identity implementation

--- app/Startup.cs
+++ app/Startup.cs
@@ -2,12 +2,34 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Identity;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 public class Startup
 {
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllersWithViews();
+
+        // Configure SQL Database connection using Azure Managed Identity
+        services.AddTransient<IDbConnection>(sp =>
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var connection = new SqlConnection(connectionString);
+            connection.AccessToken = new DefaultAzureCredential().GetToken(
+                new Azure.Core.TokenRequestContext(new[] { "https://database.windows.net/" })).Token;
+            return connection;
+        });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
