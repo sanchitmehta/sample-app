21fcf3c79d5b2d90208466c3ba004c0013b3ac78
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-02-09 12:41:51 -0800

    Update for Managed Identity implementation

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,7 @@
+using Azure.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 public class Program
 {
@@ -10,9 +12,16 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                var builtConfig = config.Build();
+                var credential = new DefaultAzureCredential();
+                config.AddAzureAppConfiguration(options =>
+                    options.Connect(builtConfig["AzureAppConfigurationConnection"])
+                           .ConfigureKeyVault(kv => kv.SetCredential(credential)));
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
             });
-
 }
