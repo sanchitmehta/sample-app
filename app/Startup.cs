ca21689e1a1f8b4b72a1a5e78091c2c217bfd076
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-02-10 06:41:15 -0800

    Update for Managed Identity implementation

--- app/Startup.cs
+++ app/Startup.cs
@@ -1,4 +1,6 @@
 using System;
+using Azure.Core;
+using Azure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +11,18 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllersWithViews();
+        string sqlConnectionString = Environment.GetEnvironmentVariable("SQLConnectionString");
+        if (!string.IsNullOrEmpty(sqlConnectionString))
+        {
+            // Using Managed Identity for SQL Server authentication
+            var defaultAzureCredential = new DefaultAzureCredential();
+            var token = defaultAzureCredential.GetToken(new TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+            // The token can be used when establishing SQL connections. 
+            // For example, when constructing a SqlConnection:
+            // var connection = new SqlConnection(sqlConnectionString);
+            // connection.AccessToken = token.Token;
+            services.AddSingleton(defaultAzureCredential);
+        }
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
