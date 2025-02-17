4f26d6dad5c5789412c87d0e9551b18737f8aad0
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-02-16 16:53:34 -0800

    Revert "Implement SQL Managed Identity Authentication"

--- app/Startup.cs
+++ app/Startup.cs
@@ -3,26 +3,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Azure.Identity;
-using Azure.Core;
-using Microsoft.Data.SqlClient;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllersWithViews();
-        var sqlConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
-        if (!string.IsNullOrEmpty(sqlConnectionString))
-        {
-            services.AddTransient<System.Data.IDbConnection>(sp =>
-            {
-                var connection = new SqlConnection(sqlConnectionString);
-                var credential = new DefaultAzureCredential();
-                connection.AccessToken = credential.GetToken(new TokenRequestContext(new[] { "https://database.windows.net/.default" })).Token;
-                return connection;
-            });
-        }
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
