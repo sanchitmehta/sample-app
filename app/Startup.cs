c7c23e00b347cd82552c271798e844914375ba0f
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-02-16 20:02:59 -0800

    Update for Managed Identity implementation

--- app/Startup.cs
+++ app/Startup.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
 
 public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllersWithViews();
+        services.AddSingleton(new DefaultAzureCredential());
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
