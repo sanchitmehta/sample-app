1378eefca92a603de9065030e84d820063f4949d
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-02-21 17:04:55 -0800

    Revert "Implement SQL Managed Identity Authentication"

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using Microsoft.Data.SqlClient;
 using SampleApp.Models;
 using System;
 using System.Collections.Generic;
-using Azure.Identity;
 
 namespace SampleApp.Controllers
 {
@@ -13,14 +12,13 @@ namespace SampleApp.Controllers
 
         public HomeController()
         {
-            // eg: Server=tcp:<server>,1433;Initial Catalog=<database-name>;Authentication=Active Directory Default;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+            // eg: Server=tcp:<server>,1433;Initial Catalog=<database-name>;Persist Security Info=False;User ID=sqladmin;Password=<password>;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
             var envConnectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING");
             if (string.IsNullOrEmpty(envConnectionString))
             {
                 throw new InvalidOperationException("Environment variable SQL_CONNECTION_STRING is not set.");
             }
             connectionString = envConnectionString;
-            var credential = new DefaultAzureCredential();
         }
 
         public IActionResult Index()
